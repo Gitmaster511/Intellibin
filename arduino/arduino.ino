a8758cf3e27b5219522245927b85d7b12c580ef6
Alaap Joshi <alaapjoshi@gmail.com>
2023-05-20 02:10:48 -0400

    asd

--- arduino/arduino.ino
+++ arduino/arduino.ino
@@ -4,7 +4,7 @@ const int echoPin = 6;
 #define GREEN_PIN 4
 #define RED_PIN 2
 #define BLUE_PIN 3
-String incomingByte = "";
+
 #include <Stepper.h>
 const int stepsPerRevolution = 2038;
 
@@ -14,6 +14,8 @@ Stepper myStepper = Stepper(stepsPerRevolution, 9, 10, 11, 12);
 
 
 float duration, distance;
+
+String string_in;
 
 void setup() {
   pinMode(trigPin, OUTPUT);
@@ -26,8 +28,7 @@ void setup() {
 }
 
 void loop() {
-  Serial.write('H'); // You can use any character or value to represent the activation signal
-  delay(1000); // Delay for 1 second
+  
   /*
   if (distance < 300) {
     digitalWrite(LED_PIN, HIGH);
@@ -43,58 +44,42 @@ void loop() {
   }
   delay(100);
   */
+
+
+  //myStepper.setSpeed(5);
+	//myStepper.step(stepsPerRevolution / 2);
+	//delay(10000);
+	
+	// Rotate CCW quickly at 10 RPM
+	//myStepper.setSpeed(30);
+	//myStepper.step(-stepsPerRevolution / 2);
+  if (Serial.available() > 0) {
+    String incomingByte;
+    incomingByte = Serial.readString(); // read the incoming byte:
+    incomingByte.trim();
+  Serial.println(incomingByte);
+  if (incomingByte == "0") {
+  Serial.println("5");
+
   myStepper.setSpeed(5);
 	myStepper.step(stepsPerRevolution / 2);
 	delay(10000);
 	
-	// Rotate CCW quickly at 10 RPM
+	//Rotate CCW quickly at 10 RPM
 	myStepper.setSpeed(30);
 	myStepper.step(-stepsPerRevolution / 2);
-  if (Serial.available() > 0) {
-  incomingByte = Serial.readString(); // read the incoming byte:
-  Serial.println(incomingByte);
-  delay(500);
-  //1000 works
-
-if(incomingByte.indexOf("Biodegradables") != -1) {
-  digitalWrite(BLUE_PIN, HIGH);
-  delay(5000);  // Delay for 5 seconds (5000 milliseconds)
-
-}
-else if (incomingByte.indexOf("Non_biodegradable-Recyclable") != -1 || incomingByte.indexOf("Non_Biodegradable-Recyclable") != -1) {
-  digitalWrite(GREEN_PIN, HIGH);
-  delay(5000);  // Delay for 5 seconds (5000 milliseconds)
-
-
-}
-else if (incomingByte.indexOf("Non_Biodegradable") != -1) {
-  digitalWrite(RED_PIN, HIGH);
-  delay(5000);  // Delay for 5 seconds (5000 milliseconds)
-
-
-}
-else if (incomingByte.indexOf("Biodegradable-Recyclable") != -1) {
-  digitalWrite(GREEN_PIN, HIGH);
-  digitalWrite(BLUE_PIN, HIGH);
-  delay(5000);  // Delay for 5 seconds (5000 milliseconds)
-
-}
-else {
-    //digitalWrite(RED_PIN, LOW);
-    //digitalWrite(GREEN_PIN, LOW);
-    //digitalWrite(BLUE_PIN, LOW);
-}
-Serial.println(incomingByte);
   }
-  digitalWrite(trigPin, LOW);
-  delayMicroseconds(2);
-  digitalWrite(trigPin, HIGH);
-  delayMicroseconds(10);
-  digitalWrite(trigPin, LOW);
+  else if (incomingByte == "1") {
+    Serial.write("1");
+  }
+  else if (incomingByte == "2") {
+    Serial.write("2");
+  }
+  else {
 
-  duration = pulseIn(echoPin, HIGH);
-  distance = (duration * .0343) / 2;
 
+  }
+  }
   //Serial.println(distance);
 
 
