a26dcf8dcc6148171c8cb654f7a181b8563317a5
Alaap Joshi <alaapjoshi@gmail.com>
2023-05-20 08:25:27 -0400

    working copy 1 with motors

--- arduino/arduino.ino
+++ arduino/arduino.ino
@@ -10,74 +10,77 @@ const int stepsPerRevolution = 2038;
 
 // Creates an instance of stepper class
 // Pins entered in sequence IN1-IN3-IN2-IN4 for proper step sequence
-Stepper myStepper = Stepper(stepsPerRevolution, 9, 10, 11, 12);
-
+Stepper Trash = Stepper(stepsPerRevolution, 9, 10, 11, 12);
+Stepper Recycle = Stepper(stepsPerRevolution, 4, 5, 6, 7);
+Stepper Bio = Stepper(stepsPerRevolution, A5, A4, A3, A2);
 
 float duration, distance;
 
 String string_in;
 
 void setup() {
+  Serial.begin(9600);
+  pinMode(A5, OUTPUT);
+  pinMode(A4, OUTPUT);
+  pinMode(A3, OUTPUT);
+  pinMode(A2, OUTPUT);
   pinMode(trigPin, OUTPUT);
   pinMode(echoPin, INPUT);
-  Serial.begin(9600);
   pinMode(LED_PIN, OUTPUT);
   pinMode(RED_PIN, OUTPUT);
   pinMode(GREEN_PIN, OUTPUT);
   pinMode(BLUE_PIN, OUTPUT);
+
+
 }
 
 void loop() {
-  
-  /*
-  if (distance < 300) {
-    digitalWrite(LED_PIN, HIGH);
-    //digitalWrite(RED_PIN, HIGH);
-    //digitalWrite(GREEN_PIN, HIGH);
-    //digitalWrite(BLUE_PIN, HIGH);
-
-  } else {
-    digitalWrite(LED_PIN, LOW);
-    digitalWrite(RED_PIN, LOW);
-    digitalWrite(GREEN_PIN, LOW);
-    digitalWrite(BLUE_PIN, LOW);
-  }
-  delay(100);
-  */
-
-
   //myStepper.setSpeed(5);
 	//myStepper.step(stepsPerRevolution / 2);
-	//delay(10000);
 	
 	// Rotate CCW quickly at 10 RPM
 	//myStepper.setSpeed(30);
 	//myStepper.step(-stepsPerRevolution / 2);
-  if (Serial.available() > 0) {
-    String incomingByte;
-    incomingByte = Serial.readString(); // read the incoming byte:
-    incomingByte.trim();
-  Serial.println(incomingByte);
-  if (incomingByte == "0") {
-  Serial.println("5");
 
-  myStepper.setSpeed(5);
-	myStepper.step(stepsPerRevolution / 2);
-	delay(10000);
-	
-	//Rotate CCW quickly at 10 RPM
-	myStepper.setSpeed(30);
-	myStepper.step(-stepsPerRevolution / 2);
-  }
-  else if (incomingByte == "1") {
-    Serial.write("1");
+
+
+
+
+
+  if (Serial.available() > 0) {
+
+    String incomingByte = Serial.readString(); // read the incoming byte:
+    //incomingByte.trim();
+    //incomingByte = incomingByte.replace("\r\n", "").replace("\r", "").replace("\n", "").replace("b", "");
+    Serial.println(incomingByte);
+
+  if (incomingByte == "0") {
+     Serial.println("Trash");
+
+  Trash.setSpeed(5);
+	Trash.step(stepsPerRevolution);
+  delay(5000);
+  Trash.setSpeed(30);
+	Trash.step(-stepsPerRevolution);
   }
   else if (incomingByte == "2") {
-    Serial.write("2");
+
+  Recycle.setSpeed(5);
+	Recycle.step(stepsPerRevolution);
+  delay(5000);
+  Recycle.setSpeed(30);
+	Recycle.step(-stepsPerRevolution);
+
+  }
+  else if (incomingByte == "1") {
+
+  Bio.setSpeed(5);
+	Bio.step(stepsPerRevolution);
+  delay(5000);
+  Bio.setSpeed(30);
+	Bio.step(-stepsPerRevolution);
   }
   else {
-
-
   }
   }
   //Serial.println(distance);
